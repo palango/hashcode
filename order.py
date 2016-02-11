1077b4a5f028ee499a17cf25d1ead3595f7295b1
Paul Lange <palango@gmx.de>
2016-02-11 19:31:39 +0100

    Add availability test for order

--- order.py
+++ order.py
@@ -2,3 +2,10 @@ class Order:
     def __init__(self, items, location):
         self.items = items
         self.location = location
+
+
+def is_ready_at(warehouse):
+    for idx, req_item in self.items.enumerate():
+        if warehouse.stock[idx] >= req_item:
+            return False
+    return True
